530226512b4c96669b2a4f380540a5289703969c
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2023-10-19 22:28:41 +0200

    projet fini

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,26 +1,21 @@
-const burger = document.querySelector('.burger');
-const navlink = document.querySelector(' nav ul');
-const header = document.querySelector('header');
-const container = document.querySelector('.container');
+const burger = document.querySelector(".burger");
+const navlink = document.querySelector(".navres");
+const header = document.querySelector("header");
+const container = document.querySelector(".container");
 
+burger.addEventListener("click", () => {
+  navlink.classList.toggle("ul-active");
+  burger.classList.toggle("toggle");
+});
 
+if (header) {
+  header.addEventListener("click", () => {
+    navlink.classList.remove("ul-active");
+    burger.classList.remove("toggle");
+  });
+}
 
-burger.addEventListener('click',()=> {
-    navlink.classList.toggle('ul-active');
-
-    burger.classList.toggle ('toggle');
-
-})
-
-if(header) {
-
-    header.addEventListener('click', ()=> {
-        navlink.classList.remove('ul-active');
-        burger.classList.remove ('toggle');
-    });
-};
-
-container.addEventListener('click', ()=> {
-    navlink.classList.remove('ul-active');
-    burger.classList.remove ('toggle');
-})
+// container.addEventListener("click", () => {
+//   navlink.classList.remove("ul-active");
+//   burger.classList.remove("toggle");
+// });
